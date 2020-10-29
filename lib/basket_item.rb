4b1b889f6a9e92ec82b3ddb66597fbedff0791cb
Edgar Kanyesigye <kedgar751@gmail.com>
2020-10-29 20:16:25 +0300

    delegate item methods/attributes to basket item

--- lib/basket_item.rb
+++ lib/basket_item.rb
@@ -1,4 +1,9 @@
+require "forwardable"
+
 class BasketItem
+  extend Forwardable
+  def_delegators :item, :name, :price, :tax
+
   attr_reader :item, :quantity
   def initialize(item, quantity)
     @item = item
--- lib/receipt.rb
+++ lib/receipt.rb
@@ -14,15 +14,15 @@ class Receipt
   private
 
   def total_sales_tax
-    basket_items.sum { |bt| bt.item.tax * bt.quantity }
+    basket_items.sum { |bt| bt.tax * bt.quantity }
   end
 
   def total_amount_plus_tax
-    basket_items.sum { |bt| bt.item.price * bt.quantity }
+    basket_items.sum { |bt| bt.price * bt.quantity }
   end
 
   def per_item(bskt_item)
-    "#{bskt_item.item.name}: #{format('%0.2f', (bskt_item.item.price + bskt_item.item.tax) * bskt_item.quantity)}"
+    "#{bskt_item.name}: #{format('%0.2f', (bskt_item.price + bskt_item.tax) * bskt_item.quantity)}"
   end
 
   def all_items
