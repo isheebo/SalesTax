a661b90eb33310b135b4bdbaad54221f5f1a8b66
Edgar Kanyesigye <kedgar751@gmail.com>
2020-10-30 07:51:45 +0300

    make small changes to aid testing

--- lib/parser.rb
+++ lib/parser.rb
@@ -10,12 +10,18 @@ def main
     create_basket_item(line_item)
   end
 
-  puts Basket.new(basket_items).generate_receipt
+  puts generate_receipt(basket_items)
+end
+
+def generate_receipt(basket_items)
+  Basket.new(basket_items).generate_receipt
 end
 
 def create_basket_item(line_item)
   quantity_item_name, price = line_item.split(/at\s+/)
   quantity = quantity_item_name.scan(/\A\d{1}/)[0].to_f
+  quantity = 1 if quantity <= 0
+
   imported = quantity_item_name.match?(/import/i)
 
   item = if quantity_item_name =~ /chocolate/i
